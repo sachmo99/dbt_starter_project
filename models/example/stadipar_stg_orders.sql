521d7c6a697968cefb7d52501bb5d0154d9ce594
Sai Chandra Mouli Tadiparthi <stadipar@cisco.com>
2022-07-28 13:36:34 +0530

    Added sample files

---
+++ models/example/stadipar_stg_orders.sql
@@ -0,0 +1,24 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+with source as (
+
+
+    select * from {{ source('public','raw_orders') }}
+
+),
+renamed as (
+
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status
+
+    from source
+
+)
+
+select * from renamed
